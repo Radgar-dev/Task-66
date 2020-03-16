11df5c01f5aaa991d042b6b19e372e3fd16f7551
Radgar-dev <Radgar-dev>
2020-03-16 12:22:27 +0100

    samuraj solution

--- main.js
+++ main.js
@@ -1,54 +1,39 @@
-const defaultTab = ["walcz", "przemyśl to jeszcze raz"]
-let resultTab = ["walcz", "przemyśl to jeszcze raz"];
+const options = ["opcja 1", "opcja 2"];
 
-const divshow = document.getElementById('show')
-
-const input = document.querySelector('input');
-const btnAdd = document.querySelector('button');
-const btnReset = document.getElementById('reset');
-const btnAdvice = document.getElementById('showAdvice');
-const btnCapa = document.getElementById('showCapabilities');
-const div = document.querySelector('div');
-
-btnAdd.addEventListener('click', (e) => {
+const addOptnios = (e) => {
     e.preventDefault();
-    const newResult = input.value;
-    if (input.value.length) {
-        for (result of resultTab) {
-            if (result === newResult) {
-                alert("Already added");
-                return
-            }
-        }
+    const input = document.querySelector('input')
+    options.push(input.value);
+    alert(`Dodana została opcja ${input.value}`);
+    input.value = ''
 
-    }
-    div.textContent += newResult + ", ";
-    resultTab.push(newResult);
-    console.log(resultTab);
-    input.value = '';
-    // alert(`Dodałeś ${newResult}`)
+}
 
-})
-
-btnReset.addEventListener('click', (e) => {
+const resetOptions = (e) => {
     e.preventDefault();
-    resultTab = [];
-    input.value = '';
-    console.log(resultTab)
-    alert("Entries reset")
-    div.textContent = '';
-})
+    options.length = 0;
+    document.querySelector('h1').textContent = "";
 
-btnCapa.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert(defaultTab + " " + resultTab)
+}
 
-})
-
-btnAdvice.addEventListener('click', (e) => {
-    e.preventDefault();
-    const randomAdvice = Math.floor(Math.random() * resultTab.length)
-    divshow.textContent = `${resultTab[randomAdvice]}`;
+const showAdvices = () => {
+    const index = Math.floor((Math.random() * options.length))
+    console.log(index)
+    document.querySelector('h1').textContent = options[index];
 
 
-})
+}
+
+const showOptions = () => {
+    alert(options.join(" "))
+
+
+}
+
+document.querySelector('.add').addEventListener('click', addOptnios)
+
+document.querySelector('.clean').addEventListener('click', resetOptions)
+
+document.querySelector('.showAdvice').addEventListener('click', showAdvices)
+
+document.querySelector('.showOptions').addEventListener('click', showOptions)
